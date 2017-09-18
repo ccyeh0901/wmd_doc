1fcb5a0db765e9bb89ef4bc6723a58d2f8e5a89b
ccyeh <ccyeh0901@gmail.com>
2017-09-18 18:54:50 +0800

    重寫 group 的migration

--- database/migrations/2017_09_16_145823_create_groups_table.php
+++ database/migrations/2017_09_16_145823_create_groups_table.php
@@ -14,10 +14,56 @@ class CreateGroupsTable extends Migration
     {
         Schema::create('groups', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->unique();
+	        $table->string('name')->nullable();  //團名
+	        $table->tinyInteger('type')->nullable(); //出團類型：中央團、教會團、工作隊
+	        $table->integer('parent_id')->unsigned()->nullable(); //多層式設計
+	        $table->integer('category_id')->unsigned()->nullable(); //屬於哪個分類
+	        $table->integer('article_id')->unsigned()->nullable(); //屬於哪篇文章
+	        $table->integer('tag_id')->unsigned()->nullable(); //屬於哪個分類
+
+	        $table->date('arriving_date')->nullable(); //入境日期
+	        $table->date('leaving_date')->nullable(); //出境日期
+	        $table->date('wmd_visit_from')->nullable(); //進入月明洞日期
+	        $table->date('wmd_visit_end')->nullable(); //離開月明洞日期
+
+	        $table->integer('estimated_number')->nullable(); // 預計人數
+//	        $table->integer('stay_days')->nullable(); // 停留天數
+	        $table->string('leader_name')->nullable(); //團長姓名
+	        $table->tinyInteger('contact_method')->nullable(); //聯絡方式
+	        $table->integer('user_id')->unsigned()->nullable(); //是誰開團的
+
+	        $table->string('schedule', 1024)->nullable();  //行程
+	        $table->text('description')->nullable(); //備註、說明...
+
+	        $table->boolean('verified')->nullable(); //是否通過審核
+	        $table->string('apply_url')->nullable(); //報名網址
 
             $table->timestamps();
         });
+
+	    Schema::create('group_tag', function (Blueprint $table) {
+		    $table->increments('id');
+		    $table->integer('group_id')->unsigned();
+		    $table->integer('tag_id')->unsigned();
+		    $table->nullableTimestamps();
+		    $table->softDeletes();
+	    });
+
+	    Schema::create('group_article', function (Blueprint $table) {
+		    $table->increments('id');
+		    $table->integer('group_id')->unsigned();
+		    $table->integer('article_id')->unsigned();
+		    $table->nullableTimestamps();
+		    $table->softDeletes();
+	    });
+
+	    Schema::create('group_category', function (Blueprint $table) {
+		    $table->increments('id');
+		    $table->integer('group_id')->unsigned();
+		    $table->integer('category_id')->unsigned();
+		    $table->nullableTimestamps();
+		    $table->softDeletes();
+	    });
     }
 
     /**
@@ -28,5 +74,10 @@ class CreateGroupsTable extends Migration
     public function down()
     {
         Schema::drop('groups');
+	    Schema::drop('group_tag');
+	    Schema::drop('group_article');
+	    Schema::drop('group_category');
+
+
     }
 }
