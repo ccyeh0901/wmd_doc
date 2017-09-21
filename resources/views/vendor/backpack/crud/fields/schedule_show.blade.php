effd5e83c70513fc3b16e7103f3d42397487ef76
ccyeh <ccyeh0901@gmail.com>
2017-09-21 22:02:44 +0800

    加入 schedule_show.blade 報名活動者可以看到的view

---
+++ resources/views/vendor/backpack/crud/fields/schedule_show.blade.php
@@ -0,0 +1,49 @@
+<!-- 開團者 選擇了什麼行程的欄位 -->
+<div @include('crud::inc.field_wrapper_attributes') >
+    <label>{!! $field['label'] !!}</label>
+    <input type="text"
+            name="{{ $field['name'] }}"
+            value="{{ old($field['name']) ? old($field['name']) : (isset($field['value']) ? $field['value'] : (isset($field['default']) ? $field['default'] : '' )) }}"
+            @include('crud::inc.field_attributes')
+    >
+
+    {{-- HINT --}}
+    @if (isset($field['hint']))
+        <p class="help-block">{!! $field['hint'] !!}</p>
+    @endif
+</div>
+
+
+@if ($crud->checkIfFieldIsFirstOfItsType($field, $fields))
+    {{-- FIELD EXTRA CSS  --}}
+    {{-- push things in the after_styles section --}}
+
+    @push('crud_fields_styles')
+        <!-- no styles -->
+
+
+        <script>
+
+            jQuery(document).ready(function($) {
+
+
+
+
+
+            });
+
+        </script>
+
+
+    @endpush
+
+
+    {{-- FIELD EXTRA JS --}}
+    {{-- push things in the after_scripts section --}}
+
+    @push('crud_fields_scripts')
+        <!-- no scripts -->
+    @endpush
+@endif
+
+{{-- Note: most of the times you'll want to use @if ($crud->checkIfFieldIsFirstOfItsType($field, $fields)) to only load CSS/JS once, even though there are multiple instances of it. --}}
