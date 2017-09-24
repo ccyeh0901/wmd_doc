a116b8b13a6fcf87cb86072ad0e769b34a82af61
ccyeh <ccyeh0901@gmail.com>
2017-09-25 05:07:38 +0800

    調整表格欄位名稱

--- app/Http/Controllers/Admin/GroupCrudController.php
+++ app/Http/Controllers/Admin/GroupCrudController.php
@@ -59,19 +59,38 @@ class GroupCrudController extends CrudController
 
 
 
-	    $this->crud->addColumn('name'); // add a text column, at the end of the stack
-	    $this->crud->addColumn('type'); // add a single column, at the end of the stack
-	    $this->crud->addColumn('wmd_visit_from'); // add a single column, at the end of the stack
+	    $this->crud->addColumn([
+		    'name' => 'name', // The db column name
+		    'label' => "團名", // Table column heading
+	    ]); // add a text column, at the end of the stack
+	    $this->crud->addColumn([
+		    'name' => 'type', // The db column name
+		    'label' => "性質", // Table column heading
+		    'type' => 'radio',
+		    'options'     => [
+			    0 => "中央團",
+			    1 => "教會團",
+			    2 => "工作隊"
+		    ]
+	    ]); // add a single column, at the end of the stack
+
+	    $this->crud->addColumn([
+		    'name' => 'estimated_number', // The db column name
+		    'label' => "總人數", // Table column heading
+	    ]); // add a t
+
+
+	    $this->crud->addColumn([
+		    'name' => 'wmd_visit_from', // The db column name
+		    'label' => "到達月明洞日期", // Table column heading
+	    ]); // add a single column, at the end of the stack
 
 	    $this->crud->addColumn([
 		    'name' => 'verified', // The db column name
 		    'label' => "是否通過審核", // Table column heading
-		    'type' => 'Radio',
-		    'options'     => [
-			    0 => "否",
-			    1 => "是"
-		    ]
+		    'type' => 'check'
 	    ]);
+	    /*以上增加表格的欄位*/
 
 
 
@@ -217,73 +236,7 @@ class GroupCrudController extends CrudController
 	    ]);
 
 
-	    // ------ CRUD FIELDS
-        // $this->crud->addField($options, 'update/create/both');
-        // $this->crud->addFields($array_of_arrays, 'update/create/both');
-        // $this->crud->removeField('name', 'update/create/both');
-        // $this->crud->removeFields($array_of_names, 'update/create/both');
 
-        // ------ CRUD COLUMNS
-        // $this->crud->addColumn(); // add a single column, at the end of the stack
-        // $this->crud->addColumns(); // add multiple columns, at the end of the stack
-        // $this->crud->removeColumn('column_name'); // remove a column from the stack
-        // $this->crud->removeColumns(['column_name_1', 'column_name_2']); // remove an array of columns from the stack
-        // $this->crud->setColumnDetails('column_name', ['attribute' => 'value']); // adjusts the properties of the passed in column (by name)
-        // $this->crud->setColumnsDetails(['column_1', 'column_2'], ['attribute' => 'value']);
-
-        // ------ CRUD BUTTONS
-        // possible positions: 'beginning' and 'end'; defaults to 'beginning' for the 'line' stack, 'end' for the others;
-        // $this->crud->addButton($stack, $name, $type, $content, $position); // add a button; possible types are: view, model_function
-        // $this->crud->addButtonFromModelFunction($stack, $name, $model_function_name, $position); // add a button whose HTML is returned by a method in the CRUD model
-        // $this->crud->addButtonFromView($stack, $name, $view, $position); // add a button whose HTML is in a view placed at resources\views\vendor\backpack\crud\buttons
-        // $this->crud->removeButton($name);
-        // $this->crud->removeButtonFromStack($name, $stack);
-        // $this->crud->removeAllButtons();
-        // $this->crud->removeAllButtonsFromStack('line');
-
-        // ------ CRUD ACCESS
-        // $this->crud->allowAccess(['list', 'create', 'update', 'reorder', 'delete']);
-        // $this->crud->denyAccess(['list', 'create', 'update', 'reorder', 'delete']);
-
-        // ------ CRUD REORDER
-        // $this->crud->enableReorder('label_name', MAX_TREE_LEVEL);
-        // NOTE: you also need to do allow access to the right users: $this->crud->allowAccess('reorder');
-
-        // ------ CRUD DETAILS ROW
-        // $this->crud->enableDetailsRow();
-        // NOTE: you also need to do allow access to the right users: $this->crud->allowAccess('details_row');
-        // NOTE: you also need to do overwrite the showDetailsRow($id) method in your EntityCrudController to show whatever you'd like in the details row OR overwrite the views/backpack/crud/details_row.blade.php
-
-        // ------ REVISIONS
-        // You also need to use \Venturecraft\Revisionable\RevisionableTrait;
-        // Please check out: https://laravel-backpack.readme.io/docs/crud#revisions
-        // $this->crud->allowAccess('revisions');
-
-        // ------ AJAX TABLE VIEW
-        // Please note the drawbacks of this though:
-        // - 1-n and n-n columns are not searchable
-        // - date and datetime columns won't be sortable anymore
-        // $this->crud->enableAjaxTable();
-
-        // ------ DATATABLE EXPORT BUTTONS
-        // Show export to PDF, CSV, XLS and Print buttons on the table view.
-        // Does not work well with AJAX datatables.
-        // $this->crud->enableExportButtons();
-
-        // ------ ADVANCED QUERIES
-        // $this->crud->addClause('active');
-        // $this->crud->addClause('type', 'car');
-        // $this->crud->addClause('where', 'name', '==', 'car');
-        // $this->crud->addClause('whereName', 'car');
-        // $this->crud->addClause('whereHas', 'posts', function($query) {
-        //     $query->activePosts();
-        // });
-        // $this->crud->addClause('withoutGlobalScopes');
-        // $this->crud->addClause('withoutGlobalScope', VisibleScope::class);
-        // $this->crud->with(); // eager load relationships
-        // $this->crud->orderBy();
-        // $this->crud->groupBy();
-        // $this->crud->limit();
     }
 
     public function store(StoreRequest $request)
