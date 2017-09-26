8541f028a66f5d126bf3e6a4790653b6fda4f181
ccyeh <ccyeh0901@gmail.com>
2017-09-26 17:48:01 +0800

    將 calendar 放到 dashboard那邊

--- app/Http/Controllers/Front/DashboardController.php
+++ app/Http/Controllers/Front/DashboardController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use Backpack\CRUD\app\Http\Controllers\CrudController;
 use Illuminate\Http\Request;
 
-class DashboardController extends Controller
+class DashboardController extends CrudController
 {
     //
 
 	public function index() {
 		$someVar = 'Some text';
-		return view('vendor.backpack.base.dashboard', compact('someVar'));
+		return view('vendor.backpack.base.calendar', compact('someVar'));
 	}
 }
--- resources/views/vendor/backpack/crud/columns/date.blade.php
+++ resources/views/vendor/backpack/crud/columns/date.blade.php
@@ -1,5 +1,4 @@
 {{-- localized date using jenssegers/date --}}
-<?php xdebug_break()?>
 <td data-order="{{ $entry->{$column['name']} }}">
     @if (!empty($entry->{$column['name']}))
 	{{ Date::parse($entry->{$column['name']})->format(config('backpack.base.default_date_format')) }}
